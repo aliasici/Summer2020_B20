32e0f07aab957de97e78a65ab82ecbd614186158
aliasici <asiciali@yahoo.com>
2020-09-02 22:54:17 -0700

    hello

---
+++ src/day50_Inheritance_Overriding/CarTask/Jeep.java
@@ -0,0 +1,17 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+start(): Call Mechanic
+         Oik change
+         jump start
+ */
+
+public class Jeep extends Car {
+
+    public void start(){
+        System.out.println("Call mechanic");
+        System.out.println("Oil change");
+        System.out.println("Jump start");
+    }
+
+
+}
---
+++ src/day50_Inheritance_Overriding/CarTask/Tesla.java
@@ -0,0 +1,11 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+Tesla:
+	start(): voice control
+ */
+
+public class Tesla extends Car{
+    public void start(){
+        System.out.println("voice control");
+    }
+}
