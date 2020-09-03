0c8d466784ff94c21dee20d780618b553a59602d
aliasici <asiciali@yahoo.com>
2020-09-02 23:02:39 -0700

    Hi

--- src/day50_Inheritance_Overriding/Test.java
+++ src/day50_Inheritance_Overriding/Test.java
@@ -1,4 +1,13 @@
 package day50_Inheritance_Overriding;
 
-public class Test {
+public class Test extends AccessModifiers {
+    public static void main(String[] args) {
+
+        System.out.println(publicData);
+        System.out.println(Test.protectedData);
+        System.out.println(Test.defaultData);
+        // System.out.println(Test.privateData);
+
+    }
+
 }
