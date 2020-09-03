0e25098cdd67eb277d34b1b2b7e898c86546ab9c
aliasici <asiciali@yahoo.com>
2020-09-02 22:55:04 -0700

    hello

--- src/day50_Inheritance_Overriding/ShapeTask/ShapeObjects.java
+++ src/day50_Inheritance_Overriding/ShapeTask/ShapeObjects.java
@@ -1,4 +1,25 @@
 package day50_Inheritance_Overriding.ShapeTask;
 
 public class ShapeObjects {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3);
+
+        System.out.println(circle.calculateArea());
+
+        System.out.println(circle.calculatePerimeter());
+
+        System.out.println("======================");
+        Rectangle rectangle = new Rectangle(3,4);
+
+        System.out.println(rectangle.calculateArea());
+
+        System.out.println(rectangle.calculatePerimeter());
+
+        System.out.println("======================");
+
+        Square square = new Square(9);
+
+        System.out.println(square.calculateArea());
+        System.out.println(square.calculatePerimeter());
+    }
 }
--- src/day50_Inheritance_Overriding/ShapeTask/Square.java
+++ src/day50_Inheritance_Overriding/ShapeTask/Square.java
@@ -1,4 +1,16 @@
 package day50_Inheritance_Overriding.ShapeTask;
 
-public class Square {
+public class Square extends Shape{
+    public double side;
+
+    public Square(double side){
+        this.side = side;
+    }
+
+    public double calculateArea(){
+        return side * side;
+    }
+    public double calculatePerimeter(){
+        return 4 * side;
+    }
 }
