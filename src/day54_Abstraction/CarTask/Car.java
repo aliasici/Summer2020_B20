bef5a927873a2ec6f18624663d0e5dccfc0ec421
aliasici <asiciali@yahoo.com>
2020-09-16 17:49:35 -0700

    Day54 Abstraction

--- src/day54_Abstraction/CarTask/Car.java
+++ src/day54_Abstraction/CarTask/Car.java
@@ -1,4 +1,43 @@
 package day54_Abstraction.CarTask;
+/*
+    Car: abstract
+        start();
+    BMW
+        start(): push start button
+    Tesla
+        start(): voice control
+    Toyota
+        start(): Insert the key
+    Jeep
+        start(): jump start
 
-public class Car {
+            Access-Modifier  abstract  Return_type   methodName(Parameter);
+            (none private)    MUST      MUST
+
+
+ */
+
+
+public abstract class Car { // cannot be FINAL
+
+    public Car(){
+
+    }
+
+    public abstract void start();
+
+
+}
+
+
+final class A{  // can be final
+
+}
+
+class B{
+    public static void main(String[] args) {
+        A obj1 = new A();
+        // Car obj2 = new Car();
+    }
+
 }
